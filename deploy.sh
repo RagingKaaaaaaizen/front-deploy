70bccb7d21de076035fd9e9a742f2a6a0a8cbd78
unknown <karlvilar000@gmail.com>
2025-08-01 15:49:35 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install dependencies
+npm install
+
+# Build the application
+npm run build
+
+# The built files will be in dist/angular-signup-verification-boilerplate/
+echo "Build completed successfully!" 
